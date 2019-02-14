847b3297e124f79d00a630aeca595ddd7b2ad54b
NathiNugget <nathaniel.risum2@gmail.com>
2019-02-14 15:35:24 +0100

    Update

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ for (x =0; x < 10 ; x++){
   console.log(x);
 
 }
+
+function yeet(){
+  conversion();
+  document.getElementById("text-box").innerHTML = (document.getElementById('indput').value);
+}
+
+function conversion(){
+  var inputValue = document.getElementById("indput");
+  if (indputValue == ("a")){
+    var indputValue = (10);
+    console.log(10)
+  }
+
+
+
+}
