26c43db0fb4e38aa7df16c83bd9754a5050a27f7
NathiNugget <nathaniel.risum2@gmail.com>
2019-03-04 11:52:33 +0100

    Update + value for letter included

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
 console.log("Hello there!");
 
-var convert = true;
-function yeet(){
+convert = true;
 
-  document.getElementById("text-box").innerHTML = (document.getElementById('indput').value);
-  if (convert = true){
-    conversion();
-  }
-  else {
-    console.log("stop")
+function conversion(inputValue){
+  var short = document.getElementById('result');
+  if (convert == true){
+    console.log("yeet");
+    if (inputValue === "A"){
+      console.log(10);
+      short.innerHTML = "Resultatet er 10"
+      convert = false;
+      console.log(convert);
+    }
+    else if (inputValue === "B"){
+      console.log(11);
+
+    }
+    else if (inputValue === "C") {
+      console.log(12);
+    }
+    else if (inputValue === "D") {
+      console.log(13);
+    }
+    else if (inputValue === "E") {
+      console.log(14);
+    }
+    else if (inputValue === "F") {
+      console.log(15);
+    }
+    else {
+      console.log("nAn");
+
+    }
   }
 }
 
-function conversion(){
-  var inputValue = document.getElementById("indput");
-  if (indputValue == ){
-    var indputValue = (10);
-    console.log(10);
-
-    var convert = false;
+function yeet(){
+  var inputValue = document.getElementById('indput').value;
+  document.getElementById("text-box").innerHTML = (document.getElementById('indput').value);
+  if (convert == true){
+    conversion(inputValue);
+    console.log("true");
   }
   else {
-    console.log("11");
-
+    console.log("stop");
   }
-  }
+}
