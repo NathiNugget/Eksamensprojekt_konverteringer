e43b465ef00f291778bf2383143ead22bfaccdfc
NathiNugget <nathaniel.risum2@gmail.com>
2019-03-04 23:30:59 +0100

    Update

---
+++ programmering_spikesolution/script.js
@@ -0,0 +1,32 @@
+console.log("Hello there!");
+var convertedValue;
+var convert = parseInt(1);
+function yeet(){
+
+  document.getElementById("text-box").innerHTML = convertedValue;
+  if (convert == 1){
+    conversion();
+  }
+  else {
+    console.log("stop");
+  }
+}
+
+function conversion(){
+  var indputValue = String(document.getElementById("indput").value);
+
+  if (convert == 1){
+    if (indputValue === "a"){
+      var convertedValue = (10);
+      console.log(10);
+      var convert = parseInt(0);
+      yeet();
+
+  }}
+  else {
+    console.log("11");
+    console.log(indputValue)
+    console.log()
+
+  }
+  }
--- script.js
+++ script.js
@@ -8,9 +8,36 @@ function conversion(inputValue){
     console.log("yeet");
     if (inputValue === "A"){
       console.log(10);
-      short.innerHTML = "Resultatet er 10"
+      short.innerHTML = "Resultatet er: 10"
       convert = false;
       console.log(convert);
+    }
+    else if (inputValue === "1") {
+      console.log(1);
+    }
+    else if (inputValue === "2") {
+      console.log(2);
+    }
+    else if (inputValue === "3") {
+      console.log(3);
+    }
+    else if (inputValue === "4") {
+      console.log(4);
+    }
+    else if (inputValue === "5") {
+      console.log(5);
+    }
+    else if (inputValue === "6") {
+      console.log(6);
+    }
+    else if (inputValue === "7") {
+      console.log(7);
+    }
+    else if (inputValue === "8") {
+      console.log(8);
+    }
+    else if (inputValue === "9") {
+      console.log(9);
     }
     else if (inputValue === "B"){
       console.log(11);
@@ -37,7 +64,7 @@ function conversion(inputValue){
 
 function yeet(){
   var inputValue = document.getElementById('indput').value;
-  document.getElementById("text-box").innerHTML = (document.getElementById('indput').value);
+  document.getElementById("text-box").innerHTML = "Dit input var: "+(document.getElementById('indput').value);
   if (convert == true){
     conversion(inputValue);
     console.log("true");
