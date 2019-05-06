1b811560bc6760535911ca1d5bad2328a8b1f01a
NathiNugget <nathaniel.risum2@gmail.com>
2019-05-06 20:02:53 +0200

    added binToDec.html && binToDec.js
    Code from the above files are integrated into index.html && website.js

--- Eksamensprojekt - IT_og_programmering/website/website.js
+++ Eksamensprojekt - IT_og_programmering/website/website.js
@@ -48,3 +48,41 @@ function yeet(){
     console.log("stop");
   }
 }
+
+// Binær --> Decimal konvertering starter her
+
+convert = true;
+
+function binConversion(binValue){
+  var binShort = document.getElementById('dec-result');
+  console.log(binValue);
+
+  var binMapping = {
+      "0" : "0",
+      "1" : "1",
+  };
+
+  var binString = binValue.toString();
+  var binReturnNum = 0;
+  var i;
+
+  for (i = 0; i < binString.length; i++) {
+      if (binMapping[binString[i]] === '1'){
+      binReturnNum += binMapping[binString[i]] * (2 ** (binString.length -1 - i));
+    }
+  }
+  console.log(binReturnNum);
+  binShort.innerHTML = "Resultat: " + binReturnNum;
+  };
+
+function binToDec(){
+  var binValue = document.getElementById('binær').value;
+  document.getElementById("bin-input").innerHTML = "Dit input var: " + (document.getElementById('binær').value);
+  if (convert == true){
+    binConversion(binValue);
+    console.log("true");
+  }
+  else {
+    console.log("stop");
+  }
+}
