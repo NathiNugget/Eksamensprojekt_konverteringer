edfe1c87ccf4f3dd935be55fb28e3700c4a5dbfc
NathiNugget <nathaniel.risum2@gmail.com>
2019-05-07 19:41:51 +0200

    Update

---
+++ Eksamensprojekt_konverteringer/website/foerste_iteration/website.js
@@ -0,0 +1,21 @@
+console.log("Hello there!")
+for (x =0; x < 10 ; x++){
+  console.log(x);
+
+}
+
+function yeet(){
+  conversion();
+  document.getElementById("text-box").innerHTML = (document.getElementById('indput').value);
+}
+
+function conversion(){
+  var inputValue = document.getElementById("indput");
+  if (indputValue == ("a")){
+    var indputValue = (10);
+    console.log(10);
+  }
+  else{
+    console.log(11);
+  }
+}
